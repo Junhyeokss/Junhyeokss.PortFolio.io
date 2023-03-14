0e71e5530723da06c2af32cabcfa95bc95689917
Junhyeokss <junhyeok0516@naver.com>
2023-03-14 17:41:30 +0900

    2023-03-14

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(".main").fullpage({
+  var FullpageOption = {
     anchors: [
       "main",
       "PortFolio01",
@@ -8,9 +8,49 @@ $(function () {
       "PortFolio04",
       "PortFolio05",
     ],
-  });
+
+    onLeave: function (origin, destination, direction) {
+      console.log(origin, destination, direction);
+      $(".gnb li")
+        .eq(destination - 1)
+        .addClass("on")
+        .siblings()
+        .removeClass("on");
+      $(".section")
+        .eq(destination - 1)
+        .addClass("on")
+        .siblings()
+        .removeClass("on");
+    },
+
+    afterRender: function () {
+      $(".gnb li").eq(0).addClass("on");
+      $(".section").eq(0).addClass("on");
+    },
+  };
+
+  $(".main").fullpage(FullpageOption);
+
+  function gnb() {
+    var gnbHeight = $(".gnb").height();
+    $(".gnb").css({
+      transform: `translate(0,${gnbHeight / 2})`,
+    });
+  }
 
   $(".mopen").on("click", function () {
     $(this).toggleClass("on");
+    $(".cover").toggleClass("on");
+    $(".header").toggleClass("on");
+  });
+
+  $(".cover li").on("click", function () {
+    $(".cover").removeClass("on");
+    $(".mopen").removeClass("on");
+    $(".header  ").removeClass("on");
+  });
+
+  $(".cover").on("wheel", function (e) {
+    e.stopPropagation();
   });
 });
